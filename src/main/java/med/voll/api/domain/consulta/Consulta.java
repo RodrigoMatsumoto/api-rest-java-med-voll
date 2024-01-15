cf3a613e361fc7ceaeeae2b8254c39736851ff51
Rodrigo Matsumoto <rodrigo.matsumoto@unesp.br>
2024-01-15 16:09:07 -0300

    criação do cancelamento de consultas

--- src/main/java/med/voll/api/domain/consulta/Consulta.java
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -2,7 +2,12 @@ package med.voll.api.domain.consulta;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.engine.spi.ExecuteUpdateResultCheckStyle;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -38,4 +43,12 @@ public class Consulta {
     private Paciente paciente;
     
     private LocalDateTime data;
+
+    @Column(name = "motivo_cancelamento")
+    @Enumerated(EnumType.STRING)
+    private MotivoCancelamento motivoCancelamento;
+
+    public void cancelar(MotivoCancelamento motivo) {
+        this.motivoCancelamento = motivo;
+    }
 }
